e4d57b8fecf1bc081a1a48c61d5cebcfe708d67c
ABHISHEK  MADAAN <abhishekmadaan2000@gmail.com>
2024-03-06 20:01:00 +0530

    updated_package.json

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,9 +1,12 @@
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 const BASE_URL = "https://wardrobe-online-backend.vercel.app/api/";
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  ?.currentUser?.accessToken;
+const TOKEN = localStorage.getItem("persist:root")?.user
+  ? JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
+      ?.currentUser?.accessToken
+  : "";
 // const TOKEN =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NmIzZjc1ZDg4YjFhNmU3YzE3NjZkYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwOTY0NTkxOH0.obTN9FC9oZpe7UpHtXz1CBZXe_DwofaEfK-h_SVnVNk";
 console.log("token---", TOKEN);
